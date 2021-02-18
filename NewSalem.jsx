1ee62fd422bab44b692b6b41dd497b962cdb356f
Muhammad Salem <mhmedsalem10@gmail.com>
2021-02-18 13:45:09 +0200

    New Salem

---
+++ NewSalem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const NewSalem = () => {
+  return (
+    <View>
+      <Text>Hello, Salem!</Text>
+    </View>
+  )
+}
+
+export default NewSalem
